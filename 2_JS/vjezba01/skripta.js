20360bb5e9ef7e9f440a7a660b7b84626d2df853
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-01-27 20:51:32 +0100

    JavaScript

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,23 @@
+console.info('Hello world 6');
+console.table({ime:"Pero", prezime:"Perić"});
+
+document.getElementById('ispis').innerText='Hello world 7';
+document.getElementById('ispis').style.color='red';
+
+document.getElementById('ue').innerHTML='<a href="#">Hello world 8</a>' ;
+
+console.log('Osnovna škola "Ivana Filipovića"');
+
+console.log('Mc\' Donalds');  //ALTGR+Q dobiješ \
+console.log("Mc 'Donalds");
+
+
+console.log('Onda radim \\ ovo'); /*u konzoli backslash*/
+
+console.log(`
+    Ovo je mojj
+    tekst
+    kroz više redova
+    `);
+
+
