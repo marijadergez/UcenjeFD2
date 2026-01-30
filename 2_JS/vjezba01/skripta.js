eb475043293d156be6e6152c1bfd0ce4aa6c6968
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-01-30 17:59:15 +0100

    js

--- 2_JS/vjezba01/skripta.js
+++ 2_JS/vjezba01/skripta.js
@@ -1,23 +1,32 @@
 console.info('Hello world 6');
 console.table({ime:"Pero", prezime:"Perić"});
 
-document.getElementById('ispis').innerText='Hello world 7';
-document.getElementById('ispis').style.color='red';
+document.getElementById('ispis').innerText='Hello world 7'; //prvo dohvati taj dokumen tkoji ima id ispis
+document.getElementById('ispis').style.color='red'; //prvo je stavio p koji je prazan onda hello world 7
+// red moze i kroz css a ovo je bio java scrpt
+
+
 
 document.getElementById('ue').innerHTML='<a href="#">Hello world 8</a>' ;
 
+console.log('')
+
 console.log('Osnovna škola "Ivana Filipovića"');
 
-console.log('Mc\' Donalds');  //ALTGR+Q dobiješ \
+console.log('Mc\' Donalds');  //  ALTGR+Q dobiješ \
 console.log("Mc 'Donalds");
 
-
+// dupli backslash \\ da bi ispisao jednu u consoli \\   
 console.log('Onda radim \\ ovo'); /*u konzoli backslash*/ /*obrnuti navodnik altgr+q*/
 
+
+//bacticks sluzi za više redova
 console.log(`    
-    Ovo je mojj
+    Ovo je moj
     tekst
     kroz više redova
     `);
+
+
 
 
--- 2_JS/vjezba02/skripta.js
+++ 2_JS/vjezba02/skripta.js
@@ -1,8 +1,55 @@
+// OPERATORI
+
+// --aritmetički   + - x
+
+const ako=2, b=3 //u jednom retku dvije varijable
+let rez //udefined
+rez = a + b //algoritam
+console.log(rez)  //izlaz   -  ulaz je podatak koji je unio korisnik 
+
+
+
+
+// --modulo operator 
+//  PARNI NEPARNI
+
+
+//ostatak nakon cjelobrojnog djeljenja 9/2 = 4,5  a cjelobrojno 4 i ostao 1
+//najednostavniji primjer je 
+console.log( 9 % 2 ) // 1       9 modulo 2
+console.log(8 % 2) //0         8 modulo 2
+
+
+//  --operatori dodjele
+// = je operator dodjeljivanja desna strana dodjeljuje lijevoj
+//prvo računa desnu stranu pa se doda varijabli sa lijeve
+//na desnoj strani rez ima vrijhednost 5 i doda mu se dva pa se 7 dodijeli varijabli sa lijeve strane
+
+rez = rez + 2
+console.log(rez) //7
+
+// + =
+
+rez += 2
+console.log(rez) //9
+
+const x =2
+
+rez -= x
+console.log(rez) //7
+
+
+
+
 const ime = prompt('Unesi svoje ime');  //skočni prozor stranice umjesto prompt mozes napisat nest drugo
 
 console.log(typeof ime, ime);
 
 console.log(`Dobar dan ${ime}!`) //bacticks altgr7
+
+
+
+
 
 
 //somuliram unos prompt:
@@ -145,4 +192,168 @@ console.log(id2 === id2);
 //tipovi podataka ,  ES6 tri načina definiranja varijabli
 //constanta je samo ime govori a varijabla je promjenjiva mutable
 //
+
+// varijabla je prostro u memoriji
+// može biti različitih tipova podataka
+// u JS ES6 postoji tri načina definiranja varijabli
+
+// ulaz podatka u program
+// prompt uvijek unosi string
+const ime = 'Tomislav'; // prompt('Unesi svoje ime');
+// simuliram da je svaki puta s pomoću prompt uneseno Tomislav
+// a to radim da ne moram svaki puta unositi
+
+console.log(typeof ime, ime);
+
+console.log(`Dobar dan ${ime}!`);
+
+const unosGodina = '2026'; // simuliram unos kroz prompt
+
+// konverzija podataka
+const godina = Number(unosGodina);
+
+console.log(typeof godina, godina+1); // ovdje uveća za 1
+console.log(typeof unosGodina, unosGodina+1); // ovdje se stringu 2026 nadoljepilo jo' 1 i dobilo 20261
+
+
+const cijeliBroj = parseInt('7');
+console.log(typeof cijeliBroj, cijeliBroj);
+
+const decimalniBroj = parseFloat('3.14'); // za decimalni dio se piše .
+
+console.log(typeof decimalniBroj, decimalniBroj);
+
+// logička vrijednost
+
+const logickaVrijednost = true; // ili false
+
+console.log(typeof logickaVrijednost, logickaVrijednost);
+
+// const se ne može mijenjati
+// logickaVrijednost = false; ovo ne može skripta.js:38 Uncaught TypeError: Assignment to constant variable.
+
+
+// ne može se redeklarirati const
+// const logickaVrijednost=false;
+
+
+// sljedeći način definiranja varijabel - pravi način let
+
+let broj = 7;
+console.log(typeof broj, broj);
+broj = 8;
+console.log(typeof broj, broj);
+// let može promijeniti tip podatka
+broj = '7';
+console.log(typeof broj, broj);
+
+// ne može se redeklarirati
+//let broj = true;
+
+// STARO, ne koristiti - što ne znači da ne postoji
+var x = 8;
+console.log(typeof x, x);
+x='8';
+console.log(typeof x, x);
+var x = true; // može se redeklarirati
+console.log(typeof x, x);
+
+
+const velikiBroj = 45421654654654654654654654n;
+console.log(typeof velikiBroj, velikiBroj);
+
+// ne mogu deklarirati konstantu bez vrijednost
+//const i;
+
+let itakonesto;
+console.log(typeof itakonesto, itakonesto);
+
+itakonesto=7;
+console.log(typeof itakonesto, itakonesto);
+
+
+let o = null;
+console.log(typeof o, o);
+o = 8;
+console.log(typeof o, o);
+
+// JSON - JavaScript Object Notation
+o = {
+    id: 1001,
+    ime: 'Pero',
+    prezime: 'Perić',
+    znaProgramirati: false
+};
+console.log(typeof o, o);
+console.table(o);
+
+// Array, niz, polje
+let niz = [2,2,3,2,2]; // ALTGR+F daje [
+console.log(typeof niz, niz); // kaže da je object
+console.table(niz);
+
+const backend = [
+    {
+        id: 1,
+        ime: 'Marina',
+        prezime: 'Generacija',
+        znaProgramirati: true
+    },
+    {
+        id: 2,
+        ime: 'Karlo',
+        prezime: 'Perić',
+        znaProgramirati: false
+    },
+    {
+        id: 3,
+        ime: 'Maja',
+        prezime: 'Katić',
+        znaProgramirati: false
+    }
+];
+console.log(typeof backend, backend);
+console.table(backend);
+
+// tijelo funckije - stari način
+function pozdrav(){
+    console.log('Pozdrav 1');
+}
+
+console.log(typeof pozdrav, pozdrav);
+// poziv funkcije
+pozdrav();
+
+
+const pozdravi = () => console.log('Pozdrav 2');
+console.log(typeof pozdravi, pozdravi);
+pozdravi();
+
+const kompleksnije = (x,y) => {
+    x = x + 3; // x = 5
+    y = --y + x; // y = 6
+    return x+y;
+};
+
+
+console.log(kompleksnije(2,2));
+
+
+const id1 = Symbol('edunova'); // edunova je opis, ali je vrijednost jedinstvena i mi ju ne znamo
+const id2 = Symbol('edunova');
+
+
+console.log(typeof id1, id2);
+
+// operator uspoređivanja 
+// ==    -> provjerava samo po vrijednosti '2' je isto što i 2
+// ===   -> provjerava i po tipu i po vrijednosti '2' nije isto što i 2
+
+console.log(id1 == id2);
+console.log(id1 === id2);
+
+console.log('2'==2); // true
+console.log('2'===2); //false
+console.log(2 === 2); //true
+
 
