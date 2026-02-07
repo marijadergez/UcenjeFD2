2533397ed3bf8323e16610b9485994cebd21e3be
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-02-07 21:01:28 +0100

    vikend zadaća html sa dva polja za unos i button za rješenje

---
+++ 2_JS/vjezba06/DZ/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btnUnesi");
+
+btn.addEventListener("click", function () {
+  const vrijednostA = document.getElementById("inputA").value;
+  const vrijednostB = document.getElementById("inputB").value;
+
+  console.log("A:", vrijednostA);
+  console.log("B:", vrijednostB);
+});
