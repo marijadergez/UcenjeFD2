6f0d8f0ee2207c3fc65ee1b4c2000d0547b38496
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-02-12 21:01:20 +0100

    vjezbanje škola

--- 2_JS/vjezba05/zad/app.js
+++ 2_JS/vjezba05/zad/app.js
@@ -149,6 +149,123 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
 });
 
+const rezultat = document.getElementById('rezultat');
+document.getElementById('izvedi').addEventListener('click', () => {
+  rezultat.innerHTML = '';
+  const a = document.getElementById('a').value;
+  const zadatak = document.getElementById('zadatak').value;
+
+  switch (zadatak) {
+    case '1':
+      // rješavanje 1. zadatak
+
+      // kraj rješavanje 1. zadatak
+    break;
+    case '2':
+      // rješavanje 2. zadatak
+
+      // kraj rješavanje 2. zadatak
+    break;
+    case '3':
+      // rješavanje 3. zadatak
+
+      // kraj rješavanje 3. zadatak
+    break;
+
+
+    /* Za uneseno ime namirnice ispisi 
+    dali je voće ili meso ili nemozes odrediti */
+
+    case '4':
+
+    if (!a){
+      rezultat.innerHTML= 'Unesite namirnicu'
+      return
+    }
+
+
+    const aCisto = a.toLowerCase()
+
+
+    if(aCisto === 'jabuka' || aCisto === 'kruška' || aCisto=== 'banana'){
+        rezultat.innerHTML= 'voće'
+    }
+      
+    else if(aCisto === 'janjetina' || aCisto=== 'teletina' || aCisto=== 'prasetina'){
+      rezultat.innerHTML= 'meso'
+      
+    }else {
+      rezultat.innerHTML='Ne možemo odrediti'
+    }
+
+
+
+
+
+    break;
+
+
+    case '5':
+
+    const doBroja = Number(a);
+    if(!doBroja){
+      rezultat.innerHTML='Nisi unio broj';
+      return;
+    }
+
+    if(doBroja<=1){
+      rezultat.innerHTML='Obavezno broj veći od 1';
+      return;
+    }
+
+    if(doBroja>10000){
+      rezultat.innerHTML='Obavezno broj manji od 10000';
+      return;
+    }
+
+    let s='';
+    for(let i=1;i<=doBroja;i++){
+      //console.log(i + ', ');
+      if(i===doBroja){
+        s += i;
+      }else{
+        s += i + ', ';
+      } 
+      //console.log(s);
+    }
+    rezultat.innerHTML = s;
+
+    break;
+
+    default:
+      rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
+  }
+
+});
+
+
+// Zadaci
+
+// 1. zadatak
+// Napiši program koji prima string komandu ("start", "stop", "pauza") i ispisuje odgovarajuću poruku.
+// Primjer ulaza: "pause"
+// Ispis: "Pauza programa"
+
+// 2. zadatak
+// Napiši program koji prima ocjenu ("A", "B", "C", "D", "F") i ispisuje:
+// "Položio" za "A", "B", "C"
+// "Nije položio" za "D" i "F"
+
+// 3. zadatak
+// Napiši program koji prima broj bodova (0–100) i ispisuje ocjenu:
+// < 50 → "Nedovoljan"
+// 50–64 → "Dovoljan"
+// 65–79 → "Dobar"
+// 80–89 → "Vrlo dobar"
+// 90–100 → "Odličan"
+// Napomena: koristi switch(true).
+
+
 
 
  
---
+++ 2_JS/vjezba07/zad/app.js
@@ -0,0 +1,214 @@
+
+const rezultat = document.getElementById('rezultat');
+document.getElementById('izvedi').addEventListener('click', () => {
+
+  const a = document.getElementById('a').value;
+  const b = document.getElementById('b').value;
+  const zadatak = document.getElementById('zadatak').value;
+
+  switch (zadatak) {
+    case '1':
+      // rješavanje 1. zadatak
+      /*Za uneseni cijeli broj u polje A veći od 2 i 
+      manji od 10M ispiši da li je prim (prosti) broj ili ne*/
+
+      let a1=Number(a)
+      console.log(a1)
+
+      if (!a1){
+        rezultat.innerHTML='Nisi unio broj'
+        return
+      }
+
+      if (a1 <2 || a1>10000000){
+        rezultat.innerHTML='Broj nije u zadanom intervalu,ponovi unos'
+        return
+      }
+      //console.log(a1)
+      /*
+      6 % 2=0
+      6 % 3=0
+      6 % 4=2
+      6 % 5=1
+      7 % 2=1
+      7 % 3=1
+      7 % 4=3
+      7 % 5=2
+      7 % 6=1  7 je prim broj jer nema nijedne nule a sest je
+      */
+      let prim =true
+
+      for ( let i = 2; i< a1; i++){
+        if(i%10000===0){       
+        console.log(a1, '%', i, '=', a1 % i)
+        }
+        if (a1 % i === 0){
+          prim = false
+          break
+        }
+      }
+
+      if(prim){
+        rezultat.innerHTML = `${a1} je prim broj`
+       }else
+        rezultat.innerHTML = `${a1} nije prim broj`
+
+ 
+      
+
+      // kraj rješavanje 1. zadatak
+    break;
+
+
+
+
+    case '2':
+      // rješavanje 2. zadatak
+
+      /*Za uneseni cijeli broj u polje B veći od 2
+       i manji od 1000 ispiši da li je savršen broj ili ne*/
+
+       const b2 =Number(b)
+        console.log(b2)
+        
+      if (!b2){
+        rezultat.innerHTML='Nisi unio broj'
+        return
+      }
+
+      if (b2 <2 || b2>1000){
+        rezultat.innerHTML='Broj nije u zadanom intervalu,ponovi unos'
+        return
+      }
+
+
+      /*
+      6 % 1 =0  +1
+      6 % 2 =0  +2
+      6 % 3 =0  +3
+      6 % 4 =2
+      6 % 5 =1
+      1+2+3 =6  zato je 6 savršen broj
+
+      5 % 1 =0
+      5 % 2 =1
+      5 % 3 =2
+      5 % 4 =1
+      1 ! = 5 zato 5 nije savršen broj
+       */
+
+      let sum = 0
+
+      for (let i= 1 ; i < b2 ; i++){
+          if(b2 % i === '0'){
+           sum += i   
+          }
+
+      }
+      if (sum === b2){
+        rezultat.innerHTML = `${b2} je savršen broj`
+      }else{
+        rezultat.innerHTML = `${b2}  nije savršen broj`
+      }
+
+     
+
+
+
+
+
+      // kraj rješavanje 2. zadatak
+    break;
+
+
+
+    
+    case '3':
+      // rješavanje 3. zadatak
+
+      // kraj rješavanje 3. zadatak
+    break;
+    case '4':
+      // rješavanje 4. zadatak
+
+      // kraj rješavanje 4. zadatak
+    break;
+    case '5':
+      // rješavanje 5. zadatak
+
+      // kraj rješavanje 5. zadatak
+    break;
+    case '6':
+      // rješavanje 6. zadatak
+
+      // kraj rješavanje 6. zadatak
+    break;
+    case '7':
+      // rješavanje 7. zadatak
+
+      // kraj rješavanje 7. zadatak
+    break;
+
+    default:
+      rezultat.innerHTML = `Nepoznati zadatak ${zadatak}`;
+  }
+
+});
+const poveznica = document.getElementById('poveznica');
+poveznica.href='https://enciklopedija.hr/clanak/primbrojevi';
+document.getElementById('zadatak').addEventListener('change', (event) => {
+   switch (event.target.value) {
+    case '1':
+      poveznica.href='https://enciklopedija.hr/clanak/primbrojevi';
+    break;
+    case '2':
+      poveznica.href='https://www.enciklopedija.hr/clanak/savrseni-broj';
+    break;
+    case '3':
+      poveznica.href='https://hafura.wordpress.com/2017/09/13/armstrongov-broj-ispitni-zadatak/';
+    break;
+    case '4':
+      poveznica.href='https://www.enciklopedija.hr/clanak/palindrom';
+    break;
+    case '5':
+      poveznica.href='https://www.coursera.org/articles/what-are-stop-words';
+    break;
+    case '6':
+      poveznica.href='https://enciklopedija.hr/clanak/samoglasnik';
+    break;
+    case '7':
+      poveznica.href='https://hjp.znanje.hr/index.php?show=search_by_id&id=d1ZlWhU%3D';
+    break;
+    default:
+      poveznica.href='#';
+  }
+});
+
+
+
+const stopRijeci = [
+  'a', 'ako', 'ali', 'bi', 'bih', 'bila', 'bili', 'bilo', 'bio', 'bismo', 
+  'biste', 'biti', 'bumo', 'da', 'do', 'duž', 'ga', 'hoće', 'hoćemo', 'hoćete', 
+  'hoćeš', 'hoću', 'i', 'iako', 'idemo', 'ideš', 'ide', 'iz', 'iza', 'iznad', 
+  'ja', 'jako', 'jer', 'jesmo', 'jeste', 'jesu', 'je', 'kad', 'kada', 'kao', 
+  'kroz', 'li', 'me', 'mene', 'meni', 'mi', 'mimo', 'mene', 'moj', 'moja', 
+  'moje', 'mu', 'na', 'nad', 'nakon', 'nam', 'nama', 'nas', 'naš', 'naša', 
+  'naše', 'našu', 'ne', 'nego', 'neka', 'neki', 'nekog', 'neko', 'nema', 'netko', 
+  'neće', 'nećemo', 'nećete', 'nećeš', 'neću', 'ni', 'nije', 'nijedan', 'nikad', 
+  'nismo', 'niste', 'nisu', 'njega', 'njegov', 'njegova', 'njegovo', 'njemu', 'njen', 
+  'njena', 'njeno', 'njih', 'njihov', 'njihova', 'njihovo', 'njim', 'njima', 'njoj', 
+  'nju', 'o', 'od', 'odmah', 'on', 'ona', 'onaj', 'onam', 'onamo', 'one', 
+  'oni', 'ono', 'onolika', 'onoliko', 'onoliki', 'onoliku', 'ova', 'ovaj', 'ovdje', 
+  'ove', 'ovi', 'ovo', 'ovuda', 'pa', 'pak', 'po', 'pod', 'pored', 'poslije', 
+  'povrh', 'preko', 'pri', 'pred', 'prema', 'sa', 'sam', 'samo', 'se', 'sebe', 
+  'sebi', 'si', 'smo', 'ste', 'su', 'sve', 'svi', 'svog', 'svoj', 'svoja', 
+  'svoje', 'ta', 'taj', 'tako', 'tamo', 'te', 'tebe', 'tebi', 'ti', 'to', 
+  'točno', 'u', 'uz', 'vam', 'vama', 'vas', 'vaš', 'vaša', 'vaše', 'već', 
+  'vi', 'vrlo', 'za', 'zar', 'će', 'ćemo', 'ćete', 'ćeš', 'ću', 'što', 'the', 
+  'and', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 
+  'had', 'do', 'does', 'did', 'but', 'if', 'or', 'because', 'as', 'until', 
+  'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 
+  'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 
+  'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
+  'further', 'then', 'once'
+];
