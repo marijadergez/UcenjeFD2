d28a14a93a0ec19d4199bfb0e3a3cee4f96175ba
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-02-20 22:47:49 +0100

    Update app.js

--- 2_JS/ljubavnikalkulator/app.js
+++ 2_JS/ljubavnikalkulator/app.js
@@ -1,7 +1,19 @@
 const ime1='Dubravka'
 const ime2='Domagoj'
 
-function izracunajLjubav(ime1, ime2) {
+function pokreniKrug(ime1, ime2) {
     const ljubav = Math.floor(Math.random() * 100) + 1;
     return ljubav;
 }
+
+const niz = [10, 20, 30, 40, 50];
+
+const prvi = niz[0];                    // prvi element
+const zadnji = niz[niz.length - 1];     // zadnji element
+
+const zbroj = prvi + zadnji;
+
+console.log(zbroj); // 60
+
+
+pokreniKrug()
