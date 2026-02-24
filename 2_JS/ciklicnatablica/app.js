5ff734e70a767d0b24be9c3c9f0fda6f74e1a34e
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-02-24 10:05:17 +0100

    igra sa divom i zbrojem

--- 2_JS/ciklicnatablica/app.js
+++ 2_JS/ciklicnatablica/app.js
@@ -1,18 +1,28 @@
+const input1 = document.getElementById("broj-input-1");
+const input2 = document.getElementById("broj-input-2");
+const button = document.getElementById("generiraj-btn");
 const rezultatDiv = document.getElementById("rezultat");
-rezultatDiv.textContent = "Tablica generirana!";
 
-const tablicaDiv = document.getElementById("tablica");
-const tablica = document.createElement("table");
-tablica.classList.add("ciklicna-tablica");
-let broj = 1;
-for (let i = 0; i < 5; i++) {
-    const red = document.createElement("tr");   
-    for (let j = 0; j < 5; j++) {
-        const celija = document.createElement("td");
-        celija.textContent = broj;
-        red.appendChild(celija);
-        broj++;
-    }
-    tablica.appendChild(red);
+function napraviDivove(broj) {
+    if (broj <= 0) return; // bazni slučaj (STOP)
+
+    const noviDiv = document.createElement("div");
+    noviDiv.textContent = broj;
+    noviDiv.style.border = "1px solid black";
+    noviDiv.style.margin = "5px";
+    noviDiv.style.padding = "5px";
+
+    rezultatDiv.appendChild(noviDiv);
+
+    napraviDivove(broj - 1); // funkcija zove samu sebe
 }
-tablicaDiv.appendChild(tablica);
+
+button.addEventListener("click", () => {
+    const broj1 = Number(input1.value);
+    const broj2 = Number(input2.value);
+    const zbroj = broj1 + broj2;
+
+    rezultatDiv.innerHTML = ""; // očisti stare
+
+    napraviDivove(zbroj);
+});
