a2088e0b80ed333b24353df10a37c316880fe7a8
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-02-24 20:19:56 +0100

    reduce filter some every

--- 2_JS/vjezba12/app.js
+++ 2_JS/vjezba12/app.js
@@ -1,11 +1,71 @@
-const korisnici=[
+const korisnici = [
 
-    { id: 1, ime: 'Ana', prezime:'Kartek', godina: 25, admin:false}
-    { id: 1, ime: 'Karlo ', prezime:'Totig', godina: 25, admin:false}
-    { id: 1, ime: 'Marko', prezime:'Ćutić', godina: 25, admin:false}
-    { id: 1, ime: 'Nikolina', prezime:'Kartek', godina: 25, admin:false}
-    { id: 1, ime: 'Ana', prezime:'Kartek', godina: 25, admin:false}
+    { id: 1, ime: 'Ana', prezime:'Kartek', godina: 25, admin: false},
+    { id: 1, ime: 'Karlo', prezime:'Totig', godina: 29, admin: false},
+    { id: 1, ime: 'Marko', prezime:'Ćutić', godina: 45, admin: true},
+    { id: 1, ime: 'Nikolina', prezime:'Ćutić', godina: 49, admin: true},
+    { id: 1, ime: 'Žana', prezime:'Đitko', godina: 17, admin: false},
 
 
-]
+];
+
+for(let i=0; i<korisnici.length; i++){
+    console.log(korisnici[i].ime)
+};
+
+console.log('********************************************************************');
+
+korisnici.forEach(korisnik => console.log(korisnik.ime));
+
+console.log('************************************************************');
+
+korisnici.forEach(o => console.log(`${o.ime}${o.prezime}`));
+
+console.log('**********************************************');
+
+korisnici.forEach(o => {
+    let oslovi = 'Poštovan'
+    if(o.ime.endsWith('a')){
+        oslovi += 'a gospođo'
+    }else{
+        oslovi += 'i gospodine'
+    }
+
+console.log(`${oslovi} ${o.ime} ${o.prezime}`)
+})
+
+
+console.log('************************************************')
+
+korisnici.forEach(o => console.log(`Poštovan${o.ime.endsWith('a') ? 'a gospođo' : 'i gospodine'} ${o.ime} ${o.prezime}`))
+
+const samoImena = korisnici.map( o=> o.ime);
+
+console.table(samoImena)
+
+const imePrezime = korisnici.map( o=> o.ime + ' ' + o.prezime)
+
+console.table(imePrezime)
+
+const maliNiz = korisnici.map(({id, ime, prezime}) => ({sifra: id, osoba: ime + ' ' + prezime}))
+
+console.table(maliNiz)
+
+console.log(korisnici.find(o => o.id === 10)?.ime)
+console.log(korisnici.find(o => o.ime === 'Ana' && o.prezime ==='Kartek').id)
+
+console.log(korisnici.findIndex(o => o.id === 1))
+
+console.table(korisnici.filter (o => o.godina > 40))
+
+console.log(korisnici.reduce((suma,o) => suma + o.godina, 0))
+
+
+console.log(korisnici.some(o => o.admin) ? 'Ima admin' : 'Nema admnin')
+
+console.log(korisnici.every( o => o.godina >= 18) ? 'Svi su punoljetni' : 'Nisu svi punoljetni')
+
+
+
+
 
