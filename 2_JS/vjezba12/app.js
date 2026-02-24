d4c50ff1661c680abec55c97a1b0d81a6e4db037
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-02-24 19:11:19 +0100

    skola

---
+++ 2_JS/vjezba12/app.js
@@ -0,0 +1,11 @@
+const korisnici=[
+
+    { id: 1, ime: 'Ana', prezime:'Kartek', godina: 25, admin:false}
+    { id: 1, ime: 'Karlo ', prezime:'Totig', godina: 25, admin:false}
+    { id: 1, ime: 'Marko', prezime:'Ćutić', godina: 25, admin:false}
+    { id: 1, ime: 'Nikolina', prezime:'Kartek', godina: 25, admin:false}
+    { id: 1, ime: 'Ana', prezime:'Kartek', godina: 25, admin:false}
+
+
+]
+
