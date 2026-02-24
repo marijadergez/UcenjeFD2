edcaf9d9b3bb175487cd20aee0e8e1b2c8a8c924
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-02-24 09:09:18 +0100

    vsc studio ponuda koda i isprobavanje što je ponudio

--- 2_JS/ciklicnatablica/app.js
+++ 2_JS/ciklicnatablica/app.js
@@ -1,2 +1,18 @@
 const rezultatDiv = document.getElementById("rezultat");
 rezultatDiv.textContent = "Tablica generirana!";
+
+const tablicaDiv = document.getElementById("tablica");
+const tablica = document.createElement("table");
+tablica.classList.add("ciklicna-tablica");
+let broj = 1;
+for (let i = 0; i < 5; i++) {
+    const red = document.createElement("tr");   
+    for (let j = 0; j < 5; j++) {
+        const celija = document.createElement("td");
+        celija.textContent = broj;
+        red.appendChild(celija);
+        broj++;
+    }
+    tablica.appendChild(red);
+}
+tablicaDiv.appendChild(tablica);
