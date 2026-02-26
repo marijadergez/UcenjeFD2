1af8cbd02ec9e08a87318c19e90a01f8dbd7d631
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-02-26 21:00:06 +0100

    povuci ponovno zad 13

--- 2_JS/vjezba13/zad/Konfiguracija.js
+++ 2_JS/vjezba13/zad/Konfiguracija.js
@@ -66,6 +66,8 @@ class Konfiguracija extends EdunovaObrada {
         };
 
         this.#projekt = izvrsiProvjeru(Pomocno.provjeriString, el.projekt, 4, 'Naziv projekta minimalno  4 znaka.');
+       
+           
         this.#instance = izvrsiProvjeru(Pomocno.provjeriCijeliBroj, el.instanci, 1, 10, 'Broj instanci mora biti između 1 i 10.');
         this.#procesor = izvrsiProvjeru(Pomocno.provjeriDecimalniBroj, el.procesor, 0.5, 'Snaga procesora mora biti min 0.5 GHz.');
         this.#datum = izvrsiProvjeru(Pomocno.provjeriDatum, el.datum, new Date(), true, 'Datum ne smije biti u prošlosti.');
