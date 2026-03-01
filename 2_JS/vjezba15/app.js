b41ab083f761992e8c29f6f9a41596150a5235c5
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-03-01 18:07:50 +0100

    kopi koda iz skjole zbog sintaksnih grešaka

--- 2_JS/vjezba15/app.js
+++ 2_JS/vjezba15/app.js
@@ -104,3 +104,68 @@ izvedi();
     })))
     .catch(rej => console.log(`%c Nije dobar URL: ${url}`,'color: red; font-size: 20px;')); // res je Response
 })();
+
+
+function primjer(prvi,drugi){
+
+}
+
+// sumuliramo čekanje funkcije
+
+function dugiPoziv(){
+    let i=0;
+    setTimeout(()=>{
+        // ovo se izvodi nakon 5 sekundi
+        console.log(++i);
+        setTimeout(() => {
+            console.log(++i);
+            setTimeout(() => {
+                console.log(++i);
+            }, 1000);
+        }, 2000);
+    }, 5000); // 5 sekundi
+}
+
+console.log('Početak');
+dugiPoziv();
+
+
+async function cekaj(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`Završio za ${ms/1000} sekundi`);
+        }, ms);
+    });
+}
+
+
+
+async function izvedi() {
+    let odgovor = await cekaj(5000);
+    console.log(odgovor);
+
+    for(let i=0;i<10;i++){
+        const ms = (i+1)*1000;
+        console.log(await cekaj(ms));
+    }
+
+}
+
+izvedi();
+
+// API: Application Programming Interface
+
+(async ()=>{
+    const url = 'https://dogapi.dog/api/v2/breeds';
+    let li;
+    await fetch(url)
+    .then(res => res.json()) // res je Response
+    .then(json => json.data.forEach(o => {
+        console.log(o.attributes.name);
+        li = document.createElement('li');
+        li.textContent = o.attributes.name;
+        document.getElementById('podaci').appendChild(li);
+    }))
+    .catch(rej => console.log(`%c Nije dobar URL: ${url}`,'color: red; font-size: 20px')); 
+})();
+
