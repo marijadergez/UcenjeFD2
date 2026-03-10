5fda8a74d3d53bdbe7b3ce17a3b9d6285dc4d64e
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-10 20:39:02 +0100

    bootstrap vite react

--- 3_REACT/helloReact/src/App.jsx
+++ 3_REACT/helloReact/src/App.jsx
@@ -1,54 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import slika from '/vite.svg'
-import lijepaSlika from './assets/slika.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import Accordion from 'react-bootstrap/Accordion';
+import slika from './assets/slika.jpg'
 
-import './App.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+   <Container>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
 
-     <img src={lijepaSlika} alt='Otok ljubavi' />
-     
-     <hr />
-   
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={slika} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='naslov'
-      style={{fontFamily: 'sans-serif'}}
-      > 
-      {count}
-       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          povećaj
-        </button>
-       
-       <button onClick={()=>{
-        const umanjeno = count - 1
-        setCount(umanjeno)
-       }}>
-        smanji
-       </button>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-       <button onClick={()=>setCount(0)} >
-        reset
-       </button>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          <img src={slika} alt='lijepaSlika'/>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+
+     <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
 
 
 
-      </div>
-      
-    </>
+   </Container>
+
   )
 }
 
