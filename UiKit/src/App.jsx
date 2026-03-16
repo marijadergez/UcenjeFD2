a4e8bd3d10b79cd8ba2f8f6433d1aaeb2a26bebd
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-03-16 19:30:58 +0100

    ui kit moj zadatak

---
+++ UiKit/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className="ticks"></div>
+
+      
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
