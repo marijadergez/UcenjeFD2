05552e35b33fe4488b812ae4cdd0b6cf96f8baae
marijadergez <45633765+marijadergez@users.noreply.github.com>
2026-03-23 20:50:19 +0100

    škola, nova ruta smjerova, ikona, button,novi file smjerovi novi ukupno priprema za dalje

--- 3_REACT/EdunovaAPP/src/App.jsx
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -8,6 +8,7 @@ import { Route, Routes } from 'react-router-dom'
 import { RouteNames } from './constants'
 import Home from './pages/Home'
 import SmjerPregled from './pages/smjerovi/SmjerPregled'
+import SmjerNovi from './pages/smjerovi/SmjerNovi'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path={RouteNames.HOME} element={<Home />}  />
         <Route path={RouteNames.SMJEROVI} element={<SmjerPregled />}  />
+        <Route path={RouteNames.SMJEROVI_NOVI} element={<SmjerNovi />}  />
       </Routes>
     <hr />
     &copy;Edunova    ||   Marija, Frontend development 2026.
--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react"
 import SmjerService from "../../services/smjerovi/SmjerService"
 import { Table } from "react-bootstrap"
 import { NumericFormat } from "react-number-format"
-import { GrValidate } from "react-icons/gr"
+import { GrAdd, GrValidate } from "react-icons/gr"
 import FormatDatuma from "../../components/FormatDatuma"
+import { Link } from "react-router-dom"
+import { RouteNames } from "../../constants"
 
 export default function SmjerPregled(){
 
@@ -22,6 +24,10 @@ export default function SmjerPregled(){
     return(
         
           <>
+          <Link to={RouteNames.SMJEROVI_NOVI}
+          className="btn btn-success w-100 my-3">
+         <GrAdd /> Dodavanje novog smjera
+          </Link>
           <Table>
             <thead>
                 <tr>
