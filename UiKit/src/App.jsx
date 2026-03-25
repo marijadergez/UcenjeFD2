9d6f2cae92a11f7c285ada2b88f0ac34d74e52ca
marija dergez <45633765+marijadergez@users.noreply.github.com>
2026-03-25 19:01:33 +0100

    nešto ne commitano iz stare učione

--- UiKit/src/App.jsx
+++ UiKit/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import React from 'react';
+
 
 
 function App() {
@@ -11,34 +11,14 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <Container>
 
-      <div className="ticks"></div>
-
-     
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      Pozdrav UiKit
+    </Container>
     </>
   )
 }
-
+ <hr />
+    &copy;Edunova
+    
 export default App
